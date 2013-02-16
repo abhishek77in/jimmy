d372ec7acbd0a6bfb43b784c9a3907c838a65f25
abhishek srivastava <abhishek77in@gmail.com>
2013-02-17 00:36:25 +0530

    working code for fetching data from SO

---
+++ StackOverflow.rb
@@ -0,0 +1,49 @@
+require 'mechanize'
+class StackOverflow
+  attr_accessor :title, :url, :excerpt
+
+  REQ_URL = "http://stackoverflow.com/search?tab=votes&q="
+  BASE_URL = "http://stackoverflow.com"
+  $QAs = Array.new
+
+  QA = ".search-result"
+  TITLE = ".result-link span"
+  URL = ".result-link span a"
+  EXCERPT = ".excerpt"
+
+  def initialize(qa)
+    @title = qa.search(TITLE).text
+    @url = BASE_URL + qa.search(URL).first["href"]
+    @excerpt = qa.search(EXCERPT).text
+    sanatize
+  end
+
+  def sanatize
+    @title = @title.gsub("\n"," ")
+    @excerpt = @excerpt.gsub("\n"," ")
+  end
+
+  def self.fetch(keyword)
+    agent = Mechanize.new
+    page = agent.get(REQ_URL + keyword)
+    page.search(QA).each do |qa|
+      $QAs << StackOverflow.new(qa)
+    end
+    log_result
+  end
+
+  def self.log_result
+    puts "Fetching complete!"
+    puts "Found #{$QAs.length} question answers."
+    puts "Listing questions/answers: "
+    $QAs.each { |i| i.to_s }
+  end
+
+  def to_s
+    puts "[#{title}](#{url})"
+    puts "#{excerpt}"
+  end
+
+end
+
+StackOverflow.fetch("rails")
